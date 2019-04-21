f88b388f6ae6e4d23c2eeb296ba7c0f7da53cd3e
cutright <dcutright@radonc.uchicago.edu>
2019-04-21 09:47:33 -0500

    calc only one stat for database columns that are already a stat of values

--- dvh-analytics-desktop/tools/stats.py
+++ dvh-analytics-desktop/tools/stats.py
@@ -24,6 +24,7 @@ class StatsData:
 
     def map_data(self):
         self.data = {}
+        stats = ['min', 'mean', 'median', 'max']
         for var in self.correlation_variables:
             if var in self.column_info.keys():
                 var_name = self.column_info[var]['var_name']
@@ -42,24 +43,45 @@ class StatsData:
                 # multi value variables
                 elif table == 'Beams':
                     src = self.table_data[table]
-                    stats = ['min', 'mean', 'median', 'max']
-                    temp = {s: [] for s in stats}
-                    for uid in self.uids:
-                        for stat in stats:
-                            values = self.get_src_values(src, var_name, uid)
-                            values = [v for v in values if v != 'None']
-                            if values:
-                                temp[stat].append(getattr(numpy, stat)(values))
-                            else:
-                                temp[stat].append(None)
 
-                    for stat in stats:
-                        corr_key = "%s (%s)" % (var, stat.capitalize())
-                        self.data[corr_key] = {'units': self.column_info[var]['units'],
-                                               'values': temp[stat]}
+                    if str_starts_with_any_in_list(var, ['Beam Complexity', 'Beam Area', 'CP MU', 'Beam Perimeter']):
+                        # stats of these four variable types have min, mean, median, and max types in DB
+                        # The following will take min, mean, median, or max of all values for a UID based on var type
+                        # Example, if var_name == Beam Complexity (Max), the following will return the Max of these
+                        temp = []
+                        for uid in self.uids:
+                            indices = self.get_beam_indices(uid)
+                            beam_data = getattr(self.table_data['Beams'], var_name)
+                            values = [beam_data[i] for i in indices if beam_data[i] != 'None']
+                            for stat in stats:
+                                if stat in var.lower():
+                                    if values:
+                                        temp.append(getattr(numpy, stat)(values))
+                                    else:
+                                        temp.append(None)
+                        self.data[var] = {'units': self.column_info[var]['units'],
+                                          'values': temp}
+                    else:
+                        temp = {s: [] for s in stats}
+                        for uid in self.uids:
+                            for stat in stats:
+                                values = self.get_src_values(src, var_name, uid)
+                                values = [v for v in values if v != 'None']
+                                if values:
+                                    temp[stat].append(getattr(numpy, stat)(values))
+                                else:
+                                    temp[stat].append(None)
+
+                        for stat in stats:
+                            corr_key = "%s (%s)" % (var, stat.capitalize())
+                            self.data[corr_key] = {'units': self.column_info[var]['units'],
+                                                   'values': temp[stat]}
 
     def get_plan_index(self, uid):
         return self.table_data['Plans'].study_instance_uid.index(uid)
+
+    def get_beam_indices(self, uid):
+        return [i for i, x in enumerate(self.table_data['Beams'].study_instance_uid) if x == uid]
 
     @staticmethod
     def get_src_values(src, var_name, uid):
@@ -77,8 +99,8 @@ class StatsData:
                 'y': self.data[y]['values']}
 
 
-# def str_starts_with_any_in_list(string_a, string_list):
-#     for string_b in string_list:
-#         if string_a.startswith(string_b):
-#             return True
-#     return False
+def str_starts_with_any_in_list(string_a, string_list):
+    for string_b in string_list:
+        if string_a.startswith(string_b):
+            return True
+    return False
